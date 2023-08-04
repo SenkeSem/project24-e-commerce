b7e15fa19cd8d3d6715f6c19cdb8966109300775
SenkeSem <sen9morf@gmail.com>
2023-08-04 21:23:16 +0300

    v1.5

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header(props) {
   return (
     <header className="d-flex justify-between align-center p-40">
       <div className="d-flex align-center">
-        <img width={40} height={40} src="/img/logo.svg" />
+        <img width={40} height={40} src="/img/logo.svg" alt="Logotype"/>
         <div>
           <h3 className="text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -10,11 +10,11 @@ function Header(props) {
       </div>
       <ul className="d-flex">
         <li className="mr-30 cu-p" onClick={props.onClickCart}>
-          <img width={18} height={18} src="/img/cart.svg" />
+          <img width={18} height={18} src="/img/cart.svg" alt="Cart" />
           <span>1205 руб.</span>
         </li>
         <li>
-          <img width={18} height={18} src="/img/user.svg" />
+          <img width={18} height={18} src="/img/user.svg" alt="User" />
         </li>
       </ul>
     </header>
